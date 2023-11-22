328354bceb95ee68254b895f2153a50649ceb419
MarkDiverta <marklee@diverta.co.jp>
2023-11-22 16:02:00 +0800

    Generate sitemap page test

---
+++ pages/generate.vue
@@ -0,0 +1,22 @@
+<template>
+<section class="p-article l-content_maxWidth-lg l-container">
+
+  <div v-if="newsTest.list.length > 0">
+    <div v-for="newsTest in newsTest.list" :key="newsTest.topics_id">
+        <NuxtLink :to="`/news/detail/${newsTest.topics_id}`">
+          {{newsTest.subject}}
+        </NuxtLink>
+    </div>
+  </div>
+
+</section>
+</template>
+
+<script setup>
+const { data: newsTest } = await useFetch(
+  'https://mtown-vercel.g.kuroco.app/rcms-api/1/content/list?cnt=60',
+  {
+    credentials: 'include',
+  }
+);
+</script>
