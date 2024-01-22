9af491f0678590d7ea532c975beef1b0e8005950
MarkDiverta <marklee@diverta.co.jp>
2024-01-22 17:46:22 +0800

    Static pages setup

---
+++ pages/about-us.vue
@@ -0,0 +1,67 @@
+<template>
+<section class="p-about l-content_maxWidth-lg l-container">
+<section class="row l-page_content-row">
+<section class="col-md-9 col-12" fluid>
+
+    <div class="l-page_content">
+      <div class="l-breadcum">
+          <a href="/" class="item">ホーム</a>
+          <i aria-hidden="true" class="icon item arrow mdi mdi-chevron-right"></i>
+          <span class="item">{{topics.title}}</span>
+      </div>
+
+      <h1 class="p-heading">{{topics.title}}</h1>
+
+      <section>
+          <div v-if="topics.desc" v-html="topics.desc"></div>
+      </section>
+
+    </div>
+
+</section>
+
+<Sidebar/>
+
+</section><!--l-page_content-row-->
+</section><!--container-fluid-->
+</template>
+
+<script setup>
+//Global setting
+const config = useRuntimeConfig(); //API route
+
+
+//API Content setting
+const apiURLBase = ref(`${config.public.kurocoApiDomain}/rcms-api/1/content/details/47639`);
+const apiURL = ref(apiURLBase.value);
+const topics = ref('[]');
+
+async function mainContent() {
+  try {
+    const response = await fetch(apiURL.value, {
+      credentials: 'include',
+    });
+    const responseData = await response.json(); //Convert to json to use on content structuring
+    const item = responseData.details;
+    let content = [];
+
+    content.date = item.ymd.substring(0, 10).replaceAll('-', '.');
+    content.title = item.subject;
+    content.desc = item.contents;
+    content.cat = item.contents_type_nm;
+    content.id = item.topics_id;
+
+    topics.value = content;
+  }
+  catch (error) {
+    console.error('Error in fetchData:', error);
+  }
+};
+mainContent();
+
+//Link function
+const goTo = (url) => {
+    window.location.href = url;
+};
+
+</script>
---
+++ pages/media.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="l-content_maxWidth-lg l-content_padding -xs l-container">
+
+    <div class="l-breadcum">
+        <!--<a href="/"><i aria-hidden="true" class="icon home item mdi mdi-home"></i></a>-->
+        <!-- <i aria-hidden="true" class="icon item arrow mdi mdi-chevron-right"></i> -->
+        <a href="/" class="item">ホーム</a>
+        <i aria-hidden="true" class="icon item arrow mdi mdi-chevron-right"></i>
+        <span class="item">{{pageName}}</span>
+    </div>
+
+    <h1 class="p-heading">{{pageName}}</h1>
+    
+    <div class="row c-block">
+        <div class="col-sm-4 col-6">
+            <div class="c-block_item --withButton">
+                <div class="c-block_thumb">
+                    <img src="https://dev-mtown.g.kuroco-img.app/files/user/klgourmesian-logo-alt-768x80.png" alt="klgourmesian-logo">
+                </div>
+                <h2 class="heading">グルメシアン Gourmesian</h2>
+                <p>マレーシアに住んでいる方/訪れている方のためのレストラン情報サイト「Gourmesian」はマレーシア国内にあるレストラン情報を多数掲載中！ 料理のジャンルや予算、エリア、クーポンのあるお店などの条件から、用途に合わせてベストなお店を検索できます！</p>
+                <a class="c-block_link c-btn c-btn_main" href="https://gourmesian.com/" target="_blank">サイトを見る</a>
+            </div>
+        </div>
+        <div class="col-sm-4 col-6">
+            <div class="c-block_item --withButton">
+                <div class="c-block_thumb">
+                    <img src="https://dev-mtown.g.kuroco-img.app/files/user/logo2.png">
+                </div>
+                <h2 class="heading">マレーシア生活情報サイト MTownlife.my</h2>
+                <p>マレーシアの基礎情報をはじめ、現地への渡航情報や交通情報、就職、ショッピング、グルメ、医療・美容情報などマレーシアで暮らすための生活情報が満載！</p>
+                <a class="c-block_link c-btn c-btn_main" href="https://malaysialife.mtown.my/" target="_blank">サイトを見る</a>
+            </div>
+        </div>
+        <div class="col-sm-4 col-6">
+            <div class="c-block_item --withButton">
+                <div class="c-block_thumb">
+                    <img src="https://dev-mtown.g.kuroco-img.app/files/user/logo1-1.png">
+                </div>
+                <h2 class="heading">マレーシア ビジネス交流サイト『CONNECTION』｜ビジネス情報源</h2>
+                <p>マレーシア進出日系企業のビジネス　コネクション作りをサポート！マレーシア進出の事前リサーチ、会社設立、人材採用、人事労務、ITサービス導入、事業拡大など各フェーズで有益な情報・知識を発信いたします。</p>
+                <a class="c-block_link c-btn c-btn_main" href="https://connection.com.my/" target="_blank">サイトを見る</a>
+            </div>
+        </div>
+    </div>
+
+  </section>
+</template>
+
+<script setup>
+//Global setting
+const config = useRuntimeConfig(); //API route
+const pageName = '関連メディア';
+
+//Link function
+const goTo = (url) => {
+    window.location.href = url;
+};
+
+</script>
