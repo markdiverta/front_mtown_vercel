c5df2c13013fd7b7c6e997c9421af9e3e09b0702
MarkDiverta <marklee@diverta.co.jp>
2024-02-07 18:41:40 +0800

    Issue list twitter

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -15,7 +15,7 @@
             <a class="l-footer_link insta" href="https://www.instagram.com/weeklymtown/" target="_blank">Instagram</a>
           </div>
           <div class="col">
-            <a class="l-footer_link twitter" href="https://twitter.com/WeeklyMtown" target="_blank">Twitter</a>
+            <a class="l-footer_link twitter" href="https://twitter.com/WeeklyMtown" target="_blank">X</a>
           </div>
           </div>
         </div>
