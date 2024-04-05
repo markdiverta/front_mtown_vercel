2c4a52c7ec2d000a6e06a1077fb516c59031b644
MarkDiverta <marklee@diverta.co.jp>
2024-04-05 16:56:22 +0800

    ISR setting changed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -70,7 +70,7 @@ export default defineNuxtConfig({
   },
   routeRules: {
     // all routes (by default) will be revalidated every 60 seconds, in the background
-    '/**': { isr: 60 }
+    '/**': { isr: 600 }
   },
 
   devtools: { enabled: true },
