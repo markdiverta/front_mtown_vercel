60dd2b0589b3196187d13d91d91c4b37093fc1b6
MarkDiverta <marklee@diverta.co.jp>
2024-05-13 13:16:22 +0800

    Clean up

--- pages/search.vue
+++ pages/search.vue
@@ -27,18 +27,11 @@ const topicID = '1';
 const route = useRoute();
 var searchKeyword;
 if (typeof route.query?.keyword != 'undefined' && route.query?.keyword != ''){
-    console.log('keyword detected');
     searchKeyword = route.query.keyword;
-
-    console.log(searchKeyword);
     if (searchKeyword.charAt(searchKeyword.length - 1) === '/') {
-        console.log('/ detected');
         searchKeyword = searchKeyword.slice(0, -1);
     };
 };
-
-console.log(searchKeyword);
-console.log(route.query);
 
 //API Content setting
 const apiURLBase = ref(`${config.public.kurocoApiDomain}/rcms-api/1/content/list?topics_keyword=${searchKeyword}`);
