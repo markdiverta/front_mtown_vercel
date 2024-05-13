94b607cad391179dbcab06cc4f248a3eaf08c904
MarkDiverta <marklee@diverta.co.jp>
2024-05-13 20:49:46 +0800

    Search stg final preview

--- components/ArticleNewsLandingStg.vue
+++ components/ArticleNewsLandingStg.vue
@@ -12,92 +12,70 @@
       </div>
 
       <h1 class="p-heading">{{pageName}}</h1>
-
-      contentChecked: {{contentChecked}}<br>
-
-      searchNotFound: {{searchNotFound}}<br>
       
       <client-only>
         
-      <UProgress animation="carousel" v-if="!contentChecked" size="sm" class="c-loadingbar"/>
+        <UProgress animation="carousel" v-if="!contentChecked" size="sm" class="c-loadingbar"/>
 
-      <section v-if="topics[0].title">
+        <section v-if="topics[0].title">
 
-        <section class="container-fluid c-blog_list" v-if="topics.length > 0">
-            <div class="row c-blog_list-item" v-for="item in topics" :key="item.id" @click="goTo(item.url)">
-                <div class="col-sm-3 col-12 thumb" :class="{ '--noIMG': !item.thumb }" :style="item.thumb ? {backgroundImage: 'url(' + item.thumb  + '?width=300)' } : ''"></div>
-                <div class="col">
-                    <h3>{{ item.title }}</h3>
-                    <div class="mb-3">
-                        <span class="date">{{ item.date }}</span>
-                        <!-- <span class="label c-label">{{ item.cat }}</span> -->
-                        <a :href="item.catURL"><span class="label c-label c-label_clickable">{{ item.cat }}</span></a>
+            <section class="container-fluid c-blog_list" v-if="topics.length > 0">
+                <div class="row c-blog_list-item" v-for="item in topics" :key="item.id" @click="goTo(item.url)">
+                    <div class="col-sm-3 col-12 thumb" :class="{ '--noIMG': !item.thumb }" :style="item.thumb ? {backgroundImage: 'url(' + item.thumb  + '?width=300)' } : ''"></div>
+                    <div class="col">
+                        <h3>{{ item.title }}</h3>
+                        <div class="mb-3">
+                            <span class="date">{{ item.date }}</span>
+                            <!-- <span class="label c-label">{{ item.cat }}</span> -->
+                            <a :href="item.catURL"><span class="label c-label c-label_clickable">{{ item.cat }}</span></a>
+                        </div>
+                        <p>{{ item.desc }}</p>
                     </div>
-                    <p>{{ item.desc }}</p>
                 </div>
-            </div>
+            </section>
+
+            <UPagination
+                v-if="pagiCount > 1"
+                v-model="page" 
+                :page-count="pageCount" 
+                :total="pagiTotal" 
+                :max="maxDisplayBtn"
+                :active-button="{ color: 'red' }"
+                class="c-pagination"
+            />
+
         </section>
-
-        <UPagination
-            v-if="pagiCount > 1"
-            v-model="page" 
-            :page-count="pageCount" 
-            :total="pagiTotal" 
-            :max="maxDisplayBtn"
-            :active-button="{ color: 'red' }"
-            class="c-pagination"
-          />
-
-      </section>
-      <section v-else-if="searchNotFound && contentChecked || contentChecked && !topics.length || contentChecked">
-
-        <template v-if="searchNotFound && contentChecked">
-            searchNotFound & contentChecked
-        </template>
-        <template v-else-if="contentChecked && !topics.length">
-            contentChecked & !topics.length
-        </template>
-        <template v-else-if="contentChecked">
-            contentChecked
-        </template>
-        <template v-else-if="searchNotFound">
-            searchNotFound
-        </template>
-        <template v-else-if="!topics.length">
-            !topics.length
-        </template>
-          
-            <template v-if="searchNotFound">
-                <p class="text-center">Couldn't find any content. Please try searching for different keywords.</p>
-                <form class="c-form row pt-4" action="/searchstg/">
-                    <div class="col-auto ml-auto">
-                        <input
-                            type="text"
-                            placeholder="ニュース検索　例：マレーシア Covid-19 感染者数"
-                            class="l-header_top-search v-input"
-                            outlined
-                            name="keyword"
-                            v-model="keyword"
-                        />
-                    </div>
-                    <div class="col-auto mr-auto">
-                        <button
-                            type="submit"
-                            block
-                            class="c-btn_main-dark c-btn submit-btn"
-                        >
-                            検索
-                        </button>
-                    </div>
-                </form>
-            </template>
-            <template v-else>
-                <p class="text-center">Sorry, content not found or is coming soon, please come back later.</p>
-            </template>
+        <section v-else-if="searchNotFound && contentChecked || contentChecked && !topics.length || contentChecked">
             
-      </section>
-
-      {{topics}}
+                <template v-if="searchNotFound">
+                    <p class="text-center">Couldn't find any content. Please try searching for different keywords.</p>
+                    <form class="c-form row pt-4" action="/searchstg/">
+                        <div class="col-auto ml-auto">
+                            <input
+                                type="text"
+                                placeholder="ニュース検索　例：マレーシア Covid-19 感染者数"
+                                class="l-header_top-search v-input"
+                                outlined
+                                name="keyword"
+                                v-model="keyword"
+                            />
+                        </div>
+                        <div class="col-auto mr-auto">
+                            <button
+                                type="submit"
+                                block
+                                class="c-btn_main-dark c-btn submit-btn"
+                            >
+                                検索
+                            </button>
+                        </div>
+                    </form>
+                </template>
+                <template v-else>
+                    <p class="text-center">Sorry, content not found or is coming soon, please come back later.</p>
+                </template>
+                
+        </section>
 
       </client-only>
 
@@ -154,22 +132,17 @@ const goTo = (url) => {
     router.push(url);
 };
 
-//API function standard
+//API function standard  *NOTED: Not using this for server side load as no static content required for dynamic search
 // apiURL.value = url ? url : apiURL.value; 
 // const response = await fetch(apiURL.value, {
 //     credentials: 'include',
 // });
 // const newsData = await response.json(); //Convert to json to use on content structuring
-// console.log('newsData');
-// console.log(newsData);
 // if (newsData.list && newsData.list.length < 1) {
-//     console.log('enter');
 //     // searchNotFound.value = isSearch ? true : false;
 //     contentChecked.value = true;
-//     console.log('enter end');
 // }
 // else if (newsData) {
-//     console.log('enter 2');
 //     let list = topics.value ? [] : topics.value;
 //     pagiTotal.value = newsData.pageInfo.totalCnt;
 //     pagiCount.value = newsData.pageInfo.totalPageCnt;
@@ -259,19 +232,16 @@ const goTo = (url) => {
 // API Content Function async for pagination
 async function fetchData(url) {
   try {
-    console.log('access');
     apiURL.value = url ? url : apiURL.value; 
     const response = await fetch(apiURL.value, {
       credentials: 'include',
     });
     const newsData = await response.json(); //Convert to json to use on content structuring
     if (newsData.list && newsData.list.length < 1) {
-        console.log('access 1');
         searchNotFound.value = isSearch ? true : false;
         contentChecked.value = true;
     }
     else if (newsData) {
-        console.log('access 2');
         let list = topics.value ? [] : topics.value;
         pagiTotal.value = newsData.pageInfo.totalCnt;
         pagiCount.value = newsData.pageInfo.totalPageCnt;
@@ -363,112 +333,6 @@ async function fetchData(url) {
 };
 //Innitial API Content Function calling
 fetchData();
-
-// NOTED: Not converting from async function as it needed for pagination (to re-run async function for pagination trigger)
-// try {
-//     // apiURL.value = url ? url : apiURL.value; 
-//     const { data: response } = await useFetch(apiURL.value,
-//         {
-//             credentials: 'include',
-//         }
-//     );
-//     const newsData = response.value;
-//     if (newsData.list && newsData.list.length < 1) {
-//         console.log('enter');
-//         searchNotFound.value = isSearch ? true : false;
-//         contentChecked.value = true;
-//     }
-//     else if (newsData) {
-//         console.log('enter 2');
-//         let list = topics.value ? [] : topics.value;
-//         console.log('access 1');
-//         console.log(newsData);
-//         pagiTotal.value = newsData.pageInfo.totalCnt;
-//         pagiCount.value = newsData.pageInfo.totalPageCnt;
-//         console.log('access 2');
-//         const content = newsData;
-//         if (!pageName) {
-//         pageName = isSubCategory ? content.list[0].contents_type_nm : content.list[0].group_nm;
-//         };
-//         console.log('access 3');
-//         parentCat = isSubCategory ? content.list[0].group_nm : '';
-//         contentChecked.value = true;
-//         for (let key in content.list) {
-//             console.log(key);
-//             const item = content.list[key];
-//             let url, thumb;
-//             let desc = item.contents;
-//             let catURL = item.category_parent_id ? catSlug + item.contents_type_slug : catSlug;
-//             desc = desc ? desc.replace(/<[^>]+>/g, '') : ''; //remove HTML
-//             if (desc && desc.length > 120) {
-//                 desc = desc.substring(0, 120);
-//                 desc += '...';
-//             };
-//             //Check if has child category or just parent category
-//             if (item.contents_type_slug && item.category_parent_id) {
-//                 url = catSlug + item.contents_type_slug + '/';
-//             } else {
-//                 url = catSlug;
-//             };
-//             //Check if is parent & has specific category props  *for columns landing page, not for news as news has parent
-//             if (isSubCategory && !item.category_parent_id) {
-//                 url += isSubCategory + '/';
-//             };
-//             //Check if has page slug else use page id
-//             if (item.slug) {
-//                 url += item.slug;
-//             } else {
-//                 url += item.topics_id;
-//             };
-//             //Thumbnail check
-//             if (item.ext_1 && item.ext_1.includes('http://') || item.ext_1 && item.ext_1.includes('https://') ) {
-//                 thumb = item.ext_1;
-//             } else if (item.ext_2 && item.ext_2.includes('http://') || item.ext_2 && item.ext_2.includes('https://') ) {
-//                 thumb = item.ext_2;
-//             };
-//             //For search page only
-//             if (isSearch) {
-//                 let path;
-//                 if (item.topics_group_id) {
-//                     let parentID = item.topics_group_id;
-//                     path = parentID == '1' ? '/news/'
-//                         : parentID == '7' ? '/eat/'
-//                         : parentID == '8' ? '/life/'
-//                         : parentID == '9' ? '/feature/'
-//                         : parentID == '10' ? '/interview/'
-//                         : parentID == '11' ? '/comics/'
-//                         : parentID == '12' ? '/community/'
-//                         : parentID == '13' ? '/malaysia-profiles/'
-//                         : parentID == '14' ? '/j-league/'
-//                         : parentID == '15' ? '/backnumber/'
-//                         : '';
-//                 };
-//                 if (path == '/news/') {
-//                     url = item.contents_type_slug ? path + item.contents_type_slug + '/' : path + 'uncategories/';
-//                 } else {
-//                     url = path;
-//                     catURL = path; //No category in others topics except news
-//                 };
-//                 url = item.slug ? url + item.slug : url + item.topics_id;
-//             };
-//             list.push({
-//                 date: item.ymd ? item.ymd.substring(0, 10).replaceAll('-', '.') : '',
-//                 title: item.subject,
-//                 desc: desc,
-//                 cat: item.contents_type_nm,
-//                 catURL: catURL,
-//                 id: item.topics_id,
-//                 url: url,
-//                 thumb: item.ext_1,
-//             });
-//         };
-//         topics.value = list;
-//         console.log(topics.value);
-//     };
-// }
-// catch (error) {
-//     console.error('An error occurred while fetching data:', error);
-// };
 
 
 //Get Category info for custom meta & page title setup
