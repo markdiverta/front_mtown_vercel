8f3ad248f90b9674bd9dc23d5751357eb6b42b1a
MarkDiverta <marklee@diverta.co.jp>
2024-07-26 18:05:54 +0800

    IP restriction troubleshoot

--- middleware/ipRestriction.global.ts
+++ middleware/ipRestriction.global.ts
@@ -12,24 +12,28 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
         const response = await axios.get('https://api.ipify.org?format=json');
         const clientIP = response.data.ip;
 
-        // console.log('clientIP check');
-        // console.log(clientIP);
+        console.log('clientIP check');
+        console.log(clientIP);
         let homeURL = '/';
         if (process.client) {
           homeURL = window.location.origin;
         };
 
-        if (!allowedIPs.includes(clientIP)) {
+        if (!allowedIPs.includes(clientIP)) { //User IP not on the allowed list
+          console.log('not allowed');
           if (to.name !== '403') {
             return navigateTo('/403', { redirectCode: 301 });
           };
-        } else {
+        } else { //User IP is allowed
+          console.log('allowed');
           if (to.name == '403') {
             return navigateTo(homeURL, { redirectCode: 301 });
           };
         };
     } catch (error) {
         console.error('Error fetching client IP:', error);
-        return navigateTo('/403');
+        if (to.name !== '403') {
+          return navigateTo('/403', { redirectCode: 301 });
+        };
     }
 });
