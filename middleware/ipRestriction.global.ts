2ffd5e0b8774219ae67b2ae7ee662bdbeea4ea04
MarkDiverta <marklee@diverta.co.jp>
2024-07-29 17:19:15 +0800

    Fix redirection

--- middleware/ipRestriction.global.ts
+++ middleware/ipRestriction.global.ts
@@ -18,18 +18,18 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 
         if (!allowedIPs.includes(clientIP)) { //User IP not on the allowed list
           console.log('not allowed');
-          // if (to.name !== '403') {
-          //   return navigateTo('/403', { redirectCode: 301 });
-          // };
+          if (to.name !== '403') {
+            return navigateTo('/403', { redirectCode: 301 });
+          };
         } else { //User IP is allowed
           console.log(to);
           console.log('allowed');
           console.log(homeURL);
           console.log(to.name);
           console.log(to.name == '403');
-          // if (to.name == '403') {
-          //   return navigateTo(homeURL, { external: process.client, redirectCode: 301 });
-          // };
+          if (to.name === '403') {
+            return navigateTo(homeURL, { external: process.client, redirectCode: 301 });
+          };
         };
     } catch (error) {
         console.log(to);
