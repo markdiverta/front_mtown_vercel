e19bd3fcce375d499560d1d7a01131bdd453502b
MarkDiverta <marklee@diverta.co.jp>
2024-07-30 14:16:11 +0800

    Complete setup IP Restriction Middleware

--- middleware/ipRestriction.global.ts
+++ middleware/ipRestriction.global.ts
@@ -13,47 +13,38 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
           const response = await axios.get('https://api.ipify.org?format=json');
           const clientIP = response.data.ip;
 
-          console.log('clientIP check');
-          console.log(clientIP);
-          let homeURL= window.location.origin;
+          // console.log('clientIP check');
+          // console.log(clientIP);
+          let homeURL= '/';
+          // let homeURL= window.location.origin;
           // let homeURL = process.client ? window.location.origin : '/';
 
           if (clientIP && !allowedIPs.includes(clientIP)) { //User IP not on the allowed list
-            console.log('not allowed');
+            // console.log('not allowed');
             if (to.name !== '403') {
-              console.log('not 403 page then go to 403');
+              // console.log('not 403 page then go to 403');
               return navigateTo('/403', { redirectCode: 301 });
             } else {
-              console.log('already 403 page then stay');
+              // console.log('already 403 page then stay');
               return true;
             };
           } else { //User IP is allowed
-            console.log(to);
-            console.log('allowed');
-            console.log(homeURL);
-            console.log(to.name);
-            console.log(to.name == '403');
+            // console.log(to);
+            // console.log('allowed');
+            // console.log(homeURL);
+            // console.log(to.name);
+            // console.log(to.name == '403');
             if (to.name === '403') {
-              console.log('is 403 page then go to home page');
-              window.location.href = homeURL;
-              // if (process.client) {
-              //   console.log('enter 1-1');
-              //   // window.location.href = homeURL;
-              // } else {
-              //   console.log('enter 1-2');
-              //   // return navigateTo('/', { redirectCode: 301 });
-              // };
-              // return navigateTo(homeURL, { external: process.client, redirectCode: 301 });
+              // console.log('is 403 page then go to home page');
+              // window.location.href = homeURL;
+              return navigateTo(homeURL, { external: process.client, redirectCode: 301 });
             } else {
-              console.log('not 304 page then remain at current page');
+              // console.log('not 304 page then remain at current page');
               return true;
             }
           };
       } catch (error) {
           console.error('Error process logic or fetch client IP:', error);
-          // if (to.name !== '403') {
-          //   return navigateTo('/403', { redirectCode: 301 });
-          // };
       }
   // }
 });
--- pages/403.vue
+++ pages/403.vue
@@ -15,34 +15,4 @@ definePageMeta({
   layout: false
 });
 
-//Global setting
-const config = useRuntimeConfig(); //API route
-const catSlug = '/news/';
-const topicID = '1';
-
-//Get search keywords
-const route = useRoute();
-var searchKeyword;
-if (typeof route.query?.keyword != 'undefined' && route.query?.keyword != ''){
-    searchKeyword = route.query.keyword;
-    if (searchKeyword.charAt(searchKeyword.length - 1) === '/') {
-        searchKeyword = searchKeyword.slice(0, -1);
-    };
-};
-
-//API Content setting
-const apiURLBase = ref(`${config.public.kurocoApiDomain}/rcms-api/1/content/search?topics_keyword=${searchKeyword}`);
-const apiURL = ref(apiURLBase.value);
-const topics = ref('[]');
-
-//If coming from footer mobile menu
-if (typeof route.query?.filter != 'undefined' && route.query?.filter.includes('topics')) {
-    apiURL.value = `${config.public.kurocoApiDomain}/rcms-api/1/content/list?topics_group_id%5B%5D=10&topics_group_id%5B%5D=7&topics_group_id%5B%5D=8`;
-    apiURLBase.value = apiURL.value;
-};
-
-//Link function
-const goTo = (url) => {
-    window.location.href = url;
-};
 </script>
