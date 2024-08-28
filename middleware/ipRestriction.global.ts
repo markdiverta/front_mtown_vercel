1351f1cf053161a79c8ed7f299e2b51078642fa1
MarkDiverta <marklee@diverta.co.jp>
2024-08-28 18:13:39 +0800

    IP restriction console

--- middleware/ipRestriction.global.ts
+++ middleware/ipRestriction.global.ts
@@ -13,14 +13,14 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
           const response = await axios.get('https://api.ipify.org?format=json');
           const clientIP = response.data.ip;
 
-          // console.log('clientIP check');
+          console.log('clientIP check');
           // console.log(clientIP);
           // let homeURL= '/';
           let homeURL= window.location.origin;
           // let homeURL = process.client ? window.location.origin : '/';
 
           if (clientIP && !allowedIPs.includes(clientIP)) { //User IP not on the allowed list
-            // console.log('not allowed');
+            console.log('not allowed');
             if (to.name !== '403') {
               // console.log('not 403 page then go to 403');
               return navigateTo('/403', { redirectCode: 301 });
@@ -30,16 +30,16 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
             };
           } else { //User IP is allowed
             // console.log(to);
-            // console.log('allowed');
+            console.log('allowed');
             // console.log(homeURL);
             // console.log(to.name);
             // console.log(to.name == '403');
             if (to.name === '403') {
-              // console.log('is 403 page then go to home page');
+              console.log('is 403 page then go to home page');
               window.location.href = homeURL;
               // return navigateTo(homeURL, { external: process.client, redirectCode: 301 });
             } else {
-              // console.log('not 304 page then remain at current page');
+              console.log('not 304 page then remain at current page');
               return true;
             }
           };
