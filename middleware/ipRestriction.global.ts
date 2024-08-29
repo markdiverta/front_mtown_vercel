f4979aab3c7318fefcb43500677499a81ab6f895
MarkDiverta <marklee@diverta.co.jp>
2024-08-29 12:45:20 +0800

    IP update

--- middleware/ipRestriction.global.ts
+++ middleware/ipRestriction.global.ts
@@ -6,7 +6,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     const allowedIPs = 
       [
           '123.45.67.89', 
-          '161.142.139.157'   //Mark
+          '161.142.139.160'   //Mark
       ];
     // console.log('access');
       try {
